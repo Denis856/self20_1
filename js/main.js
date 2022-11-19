f60c725d10fff5ec112dccbaeabcc0bea3eb08dd
Den89 <malyhindenis8@gmail.com>
2022-11-19 17:44:14 +0500

    fsdf

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ $(".main").click(function(){
    $(".menu1__list").removeClass('turn2');
 });
 
+// =======================================================
+
 $(function(){
    $('a[href^="#"]').click(function(){
    var target = $(this).attr('href');
@@ -21,6 +23,8 @@ $(function(){
    return false;
    });
    });
+
+// =======================================================
 
 
    function consoleBG() {
@@ -39,32 +43,3 @@ $(function(){
 
 
 // =========================================
-
-(function($) {
-	function animatedBlocks () {
-		$('.need-animate').each(function () {
-			var scrollPosTop = $(window).scrollTop(), // позиция скролла, верх страницы
-				blockPosTop = $(this).offset().top, // позиция блока (верх), который нужно анимировать
-				blockPosBottom = blockPosTop+$(this).height(), // позиция блока (низ), который нужно анимировать
-				windowHeight = $(window).height(), // высота окна браузера
-				windowLineTop = scrollPosTop+(windowHeight*(1/8)), // верхняя граница окна браузера при пересечении которой анимируется блок
-				windowsLineBottom = scrollPosTop+(windowHeight*(7/8)); // нижяя граница окна браузера при пересечении которой анимируется блок
-
-			if (
-				(windowLineTop <= blockPosBottom && windowLineTop > blockPosTop) // если верхняя часть экрана выше нижней границы блока, но не прошла блок полностью
-				|| (windowsLineBottom >= blockPosTop && windowsLineBottom < blockPosBottom) // если нижняя часть экрана дошла до верхней границы блока, но не прошла блок полностью
-				|| ((blockPosTop > windowLineTop && blockPosTop < windowsLineBottom) || (blockPosBottom > windowLineTop && blockPosBottom < windowsLineBottom)) // или блок по центру экрана
-			) {
-				$(this).removeClass('need-animate').addClass('animated');
-			}
-		});
-	}
-	$(document).ready(function () {
-		setTimeout(function () {
-			animatedBlocks();
-		}, 1000);
-	});
-	$(window).scroll(function () {
-		animatedBlocks();
-	});
-})(jQuery);
